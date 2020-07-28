9e240d985a7dcb8b7fa4fa747be31a89f2abae59
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-07-29 00:19:26 +0600

    #4 village data taken by a form

--- solutions/65-niloy/src/main/Controller.java
+++ solutions/65-niloy/src/main/Controller.java
@@ -3,7 +3,11 @@ package main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 
 
 public class Controller {
@@ -26,13 +30,41 @@ public class Controller {
     private Button openVillage;
 
     @FXML
-    void createNewVillage(ActionEvent event) {
+    private AnchorPane drawingSpace;
 
+    @FXML
+    private AnchorPane infoLayout;
+
+    @FXML
+    private TextField inputVillageName;
+
+    @FXML
+    private TextField inputNationName;
+
+    @FXML
+    private Button createVillage;
+
+
+    @FXML
+    private Label villageName;
+
+    @FXML
+    private Label nationName;
+
+    @FXML
+    void createVillage(ActionEvent event) {
+        villageName.setText(inputVillageName.getText());
+        nationName.setText(inputNationName.getText());
+        infoLayout.setVisible(false);
+    }
+
+    @FXML
+    void newVillage(ActionEvent event) {
+        infoLayout.setVisible(true);
     }
 
     @FXML
     void openVillage(ActionEvent event) {
-
     }
 
     @FXML
