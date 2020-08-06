7edf38c15e1929abbc6fc98405a778255a674c04
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-08-06 23:12:27 +0600

    #9 Bangladeshi Farmer class created

---
+++ solutions/65-niloy/src/nation/BangladeshiFarmers.java
@@ -0,0 +1,23 @@
+package nation;
+
+public class BangladeshiFarmers implements INation{
+    @Override
+    public void drawTerrain() {
+        
+    }
+
+    @Override
+    public void drawHouse() {
+
+    }
+
+    @Override
+    public void drawTree() {
+
+    }
+
+    @Override
+    public void drawRiver() {
+
+    }
+}
--- solutions/65-niloy/src/nation/INation.java
+++ solutions/65-niloy/src/nation/INation.java
@@ -1,2 +1,8 @@
-package nation;public interface INation {
+package nation;
+
+public interface INation {
+    void drawTerrain();
+    void drawHouse();
+    void drawTree();
+    void drawRiver();
 }
