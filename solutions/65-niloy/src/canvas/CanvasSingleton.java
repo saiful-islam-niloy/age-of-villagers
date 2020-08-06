cbe97749a50432f9294093058788d492038314d4
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-08-06 23:39:30 +0600

    #4 canvas singleton class created for getting same canvas object

---
+++ solutions/65-niloy/src/canvas/CanvasSingleton.java
@@ -0,0 +1,22 @@
+package canvas;
+
+import javafx.scene.canvas.Canvas;
+
+public class CanvasSingleton {
+    private static CanvasSingleton instance = null;
+    private Canvas canvas;
+
+    private CanvasSingleton() {
+        canvas = new Canvas(600, 400);
+    }
+
+    public static CanvasSingleton getInstance() {
+        if (instance == null)
+            instance = new CanvasSingleton();
+        return instance;
+    }
+
+    public Canvas getCanvas(){
+        return canvas;
+    }
+}
