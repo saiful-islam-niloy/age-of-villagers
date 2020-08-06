80b385f8807fbc469db1e91ac4ab1c2c6f9346ee
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-08-06 23:24:54 +0600

    #8 terrain interface created

--- solutions/65-niloy/src/nation/NationManager.java
+++ solutions/65-niloy/src/nation/NationManager.java
@@ -8,6 +8,11 @@ public class NationManager {
     }
 
     public void draw(){
-
+        NationFactory nationFactory = new NationFactory();
+        INation nation = nationFactory.getNation(nationType);
+        nation.drawTerrain();
+        nation.drawHouse();
+        nation.drawTree();
+        nation.drawRiver();
     }
 }
