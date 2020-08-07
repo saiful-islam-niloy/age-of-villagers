f0783a8545f5e7458f734490a16148c8ddbb1d0d
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-08-07 11:33:51 +0600

    #10 Arab Bedouin class created

---
+++ solutions/65-niloy/src/nation/ArabBedouin.java
@@ -0,0 +1,28 @@
+package nation;
+
+import house.IHouse;
+import river.IWaterSource;
+import terrain.ITerrain;
+import tree.ITree;
+
+public class ArabBedouin implements INation{
+    @Override
+    public ITerrain getTerrain() {
+        return null;
+    }
+
+    @Override
+    public IHouse getHouse() {
+        return null;
+    }
+
+    @Override
+    public ITree getTree() {
+        return null;
+    }
+
+    @Override
+    public IWaterSource getWaterSource() {
+        return null;
+    }
+}
