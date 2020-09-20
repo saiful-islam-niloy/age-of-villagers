0f15a1f98491cc83c1bb2005cf739d94bee44a70
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-09-20 09:46:19 +0600

    #1 half-cirle shape refactored

--- solutions/65-niloy/src/shape/HalfCircle.java
+++ solutions/65-niloy/src/shape/HalfCircle.java
@@ -1,21 +1,22 @@
 package shape;
 
+import canvas.CanvasSingleton;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.ArcType;
 
 public class HalfCircle implements IShape{
     private Point center;
     private int diameter;
-    private GraphicsContext graphicsContext;
 
-    public HalfCircle(Point center, int diameter, GraphicsContext graphicsContext) {
+    public HalfCircle(Point center, int diameter) {
         this.center = center;
         this.diameter = diameter;
-        this.graphicsContext = graphicsContext;
     }
 
     @Override
     public void draw() {
+        CanvasSingleton canvasSingleton = CanvasSingleton.getInstance();
+        GraphicsContext graphicsContext = canvasSingleton.getCanvas().getGraphicsContext2D();
         graphicsContext.strokeArc(center.x, center.y, diameter, diameter, 0, 180, ArcType.ROUND);
     }
 }
