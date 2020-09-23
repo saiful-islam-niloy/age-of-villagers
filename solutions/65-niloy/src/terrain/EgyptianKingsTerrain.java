34b36427f0f8daf1cade5f5850f512a971e061f4
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-09-23 07:25:45 +0600

    #1 egyptian and inuit terrain refactored

--- solutions/65-niloy/src/terrain/EgyptianKingsTerrain.java
+++ solutions/65-niloy/src/terrain/EgyptianKingsTerrain.java
@@ -5,11 +5,13 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class EgyptianKingsTerrain implements ITerrain{
+    private String colorCode;
     @Override
     public void draw() {
         CanvasSingleton canvasSingleton = CanvasSingleton.getInstance();
         GraphicsContext graphicsContext = canvasSingleton.getCanvas().getGraphicsContext2D();
-        graphicsContext.setFill(Color.web("#ffc80d"));
+        colorCode = "#ffc80d";
+        graphicsContext.setFill(Color.web(colorCode));
         graphicsContext.fillRect(0,0,600,400);
     }
 }
--- solutions/65-niloy/src/terrain/InuitHuntersTerrain.java
+++ solutions/65-niloy/src/terrain/InuitHuntersTerrain.java
@@ -5,11 +5,13 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class InuitHuntersTerrain implements ITerrain {
+    private String colorCode;
     @Override
     public void draw() {
         CanvasSingleton canvasSingleton = CanvasSingleton.getInstance();
         GraphicsContext graphicsContext = canvasSingleton.getCanvas().getGraphicsContext2D();
-        graphicsContext.setFill(Color.web("#ffffff"));
+        colorCode = "#ffffff";
+        graphicsContext.setFill(Color.web(colorCode));
         graphicsContext.fillRect(0,0,600,400);
     }
 }
